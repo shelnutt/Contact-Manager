89085c4330588866d268d339081e687eed05f722
NJamison1 <84431644+NJamison1@users.noreply.github.com>
2022-01-21 11:09:02 -0500

    Add files via upload

---
+++ LAMPAPI/AddContact.php
@@ -0,0 +1,44 @@
+<?php
+	$inData = getRequestInfo();
+
+	$userId = $inData["userId"];
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$Email = $inData["Email"];
+	$Phone = $inData["Phone"];
+	$today = date("Y-m-d H:i:s"); 
+	$DateCreated = $today;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); //does this need to change based on logged in user?
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (UserId,FirstName,LastName,Email,Phone,DateCreated) VALUES(?,?,?,?,?,?)");
+		$stmt->bind_param("ss", $userId, $FirstName, $LastName, $Email, $Phone, $DateCreated);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
---
+++ LAMPAPI/Register.php
@@ -0,0 +1,44 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$Login = $inData["Login"];
+	$Password = $inData["Password"];
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$today = date("Y-m-d H:i:s"); 
+	$DateCreated = $today;// get current date
+	$DateLastLoggedIn = $today; // get current date
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Users (Login,Password,FirstName,LastName,DateCreated,DateLastLoggedIn) VALUES(?,?,?,?,?,?)");
+		$stmt->bind_param("ss", $Login, $Password, FirstName, LastName, DateCreated, DateLastLoggedIn);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
