e28ab85f03e8ef084a8dc016b149fa2d8da932b7
Jorge Bulnes <97678857+jbulnes96@users.noreply.github.com>
2022-01-25 13:54:25 -0500

    Add files via upload

---
+++ LAMPAPI/DeleteContacts.php
@@ -0,0 +1,41 @@
+<?php
+	$inData = getRequestInfo();
+
+	$userId = $inData["userId"];
+	$FirstName = $inData["FirstName"];
+	$LastName = $inData["LastName"];
+	$Email = $inData["Email"];
+	$Phone = $inData["Phone"];
+	$today = date("Y-m-d H:i:s"); 
+	$DateCreated = $today;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else 
+	{
+		$sql = "DELETE FROM contacts where id=1";
+		printf('Data deleted successfully');
+		
+	}	
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
