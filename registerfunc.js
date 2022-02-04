ac92377947be38c8ae5e36e323b73a527b4c1691
Shelnutt <bcshelnutt@knights.ucf.edu>
2022-02-03 19:19:16 -0500

    register

--- registerfunc.js
+++ registerfunc.js
@@ -1,18 +1,21 @@
 const urlBase = "http://contactman4331.xyz/LAMPAPI";
 const extension = "php";
 
+let userId = 0;
+
 
 function register()
 {
-	
-	let fName = document.getElementById("firstName").value;
-	let lName = document.getElementById("lastName").value;
+
+	userId = 0;	
+	let firstName = document.getElementById("firstName").value;
+	let lastName = document.getElementById("lastName").value;
 	let login = document.getElementById("loginName").value;
 	let password = document.getElementById("loginPassword").value;
 
 	document.getElementById("loginResult").innerHTML = "";
 
-	let temp = {FirstName:fName,LastName:lName,Login:login,Password:password};
+	let temp = {Login:login,Password:password,FirstName:firstName,LastName:lastName};
 	let jsonPayload = JSON.stringify(temp);
 
 	let url = urlBase + "/Register." + extension;
@@ -22,6 +25,15 @@ function register()
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				let jsonObject = JSON.parse(xhr.responseText);
+				userId = jsonObject.id;
+				window.location.href = "index.html";
+			}
+		};
 		xhr.send(jsonPayload);
 	}
 	catch(err)
